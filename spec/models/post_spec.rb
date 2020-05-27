ee6f69f746a2fa55fa5dbd62086e31a82bac39b8
Beth Ruff <beth.ruff2@gmail.com>
2020-05-27 20:00:00 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
  
 describe Post do
  
+<<<<<<< HEAD
  it 'can be created' do
     post = Post.create!(title: "My title", description: "The post description")
     expect(post).to be_valid
@@ -12,4 +13,6 @@ describe Post do
     expect(post.post_summary).to eq("My title - The post description")
   end 
   
+=======
+>>>>>>> 4b918931999f92eff8dff47e4e3a7ba74ab8c604
 end
